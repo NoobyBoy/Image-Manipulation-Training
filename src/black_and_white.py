820c648f79db5d15656c955df33125bb6707c011
Timothée Cavalli <timothee.cavalli@epitech.eu>
2018-10-12 11:56:33 +0200

    comment on black_and_white

--- src/black_and_white.py
+++ src/black_and_white.py
@@ -6,8 +6,22 @@ from PIL import Image
 from statistics import mean
 import os
 
+"""
+This module contain the print_function:
+
+-black_and_white :
+    Transform the image passed as parameter in a black and white Image,
+    it work pretty well but is very looow...
+    Have to find a way to do it faster
+
+"""
+
 
 def black_and_white(img):
+    """
+    for each pixel of the img the average of it's RGB component is applied to
+    each RGB component wich result in a darker or brighter grey
+    """
 
     px = img.load()
     size_x, size_y = img.size
@@ -21,9 +35,9 @@ def black_and_white(img):
 
 
 
-
 if __name__ == "__main__":
-    img = Image.open("../image/space.jpg")
+    img = Image.open("../image/spidey.jpg")
     black_and_white(img)
+    img.show()
     print ("Black and White : \n")
     os.system("pause")
