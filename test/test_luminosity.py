809b30eeac47b37704713b0a3000c2d5ca4db19b
Timothée Cavalli <timothee.cavalli@epitech.eu>
2018-10-16 15:10:24 +0200

    other test
    seems to work ...

--- test/test_luminosity.py
+++ test/test_luminosity.py
@@ -40,5 +40,29 @@ class LuminosityTest(unittest.TestCase):
                 self.assertEqual(px3[j,i], (255,255,255))
 
 
+    def test_luminosity_percentage(self):
+
+                img = Image.open("../image/spidey.jpg")
+                img_clean = img.copy()
+                img2 = Image.open("../image/spidey.jpg")
+                img3 = Image.open("../image/spidey.jpg")
+                x,y = img.size
+
+                luminosity_percentage(img, 50)
+                luminosity_percentage(img2, 0)
+                luminosity_percentage(img3, 100)
+
+                px = img.load()
+                px_clean = img_clean.load()
+                px2 = img.load()
+                px3 = img3.load()
+
+                for i in range (y):
+                    for j in range (x):
+                        print(j,i, px2[j,i])
+                        self.assertTrue(px[j,i], px_clean[j,i])
+                        self.assertTrue(px2[j,i], (0, 0, 0))
+                        self.assertEqual(px3[j,i], (255,255,255))
+
 if __name__ == '__main__':
     unittest.main()
