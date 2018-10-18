7c0e059a59111e5359ce1cd75bbf421da93075e7
Timothée Cavalli <timothee.cavalli@epitech.eu>
2018-10-18 12:29:29 +0200

    unittest fixed

--- test/test_black_and_white.py
+++ test/test_black_and_white.py
@@ -14,17 +14,11 @@ class TestBlackAndWhite(unittest.TestCase):
         img = Image.open("../image/spidey.jpg")
         img2 = Image.open("../image/spidey.jpg")
 
-        px = img.load()
-        px2 = img2.load()
-
         #Same as test_shuffling can't test directly if equal
         #self.assertEqual(px, px2)
 
         black_and_white(img)
         GreyScale(img2)
-
-        px = img.load()
-        px2 = img2.load()
 
         #self.assertEqual(px, px2)
 
--- test/test_shuffling.py
+++ test/test_shuffling.py
@@ -17,25 +17,35 @@ class TestShuffling(unittest.TestCase):
         img2 = Image.open("../image/spidey.jpg")
         img3 = Image.open("../image/spidey.jpg")
 
-        px = img.load()
-        px2 = img2.load()
-        px3 = img3.load()
+        size_x, size_y = img.size
 
-        # Don't work :/ can't test directly if equal
-        self.assertEqual(px, px2)
-        self.assertEqual(px, px3)
-        self.assertEqual(px2, px3)
+        for y in range(size_y):
+            for x in range(size_x):
+                self.assertEqual(img.getpixel((x,y)), img2.getpixel((x,y)))
+                self.assertEqual(img.getpixel((x,y)), img3.getpixel((x,y)))
+                self.assertEqual(img2.getpixel((x,y)), img3.getpixel((x,y)))
 
         shuffling(img2, 10)
         shuffling(img3, 10)
 
-        px2 = img2.load()
-        px3 = img3.load()
 
-        #The probability that one of them are equal is veeeeery low
-        self.assertNotEqual(px, px2)
-        self.assertNotEqual(px, px3)
-        self.assertNotEqual(px2, px3)
+        #very slow way to test but testing each pixel independently is not
+        # a good idea (some pixel may be identical but the picture is
+        #  not the same)
+        l1 = []
+        l2 = []
+        l3 = []
+        for y in range(size_y):
+            for x in range(size_x):
+                l1.append(img.getpixel((x,y)))
+                l2.append(img2.getpixel((x,y)))
+                l3.append(img3.getpixel((x,y)))
+            self.assertNotEqual(l1, l2)
+            self.assertNotEqual(l1, l3)
+            self.assertNotEqual(l2, l3)
+            l1 = []
+            l2 = []
+            l3 = []
 
 
 if __name__ == '__main__':
