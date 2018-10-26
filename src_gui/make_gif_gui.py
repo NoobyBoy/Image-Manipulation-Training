f0fa507a8464be54b8cd189d8acfd3762b0a9dbd
Timothée Cavalli <timothee.cavalli@epitech.eu>
2018-10-26 14:21:53 +0200

    gif maker window front
    don't work just the front

---
+++ src_gui/make_gif_gui.py
@@ -0,0 +1,59 @@
+#-*- conding utf-8 -*-
+
+from tkinter import *
+from tkinter import ttk
+
+class MyDialogGif:
+
+    def __init__(self, parent):
+
+        self.top = Toplevel(parent)
+        self.top.grab_set()
+        self.top.title("Make your GIF")
+
+        self.result = (None, None, None, None)
+        self.img_list = []
+        self.path = ""
+        self.duration = 100
+        self.loop_mode = 0
+
+        self.can_but = Canvas(self.top)
+
+        self.can_img = Canvas(self.can_but, width=150, height=150, bg="white")
+        self.but_add = Button(self.can_but, text="Add Picture(s) to selection")
+        self.lab_dur = Label(self.can_but, text="Duration of each frame\n(milliseconds) :")
+        self.ent_dur = Entry(self.can_but, width=5)
+        self.lab_loop = Label(self.can_but, text="Number of loop\n (0 = infinite) :")
+        self.ent_loop = Entry(self.can_but, width=5)
+        self.but_save = Button(self.can_but, text="Save GIF")
+        self.but_cancel = Button(self.can_but, text="Cancel")
+
+        self.ent_dur.insert(0, str(self.duration))
+        self.ent_loop.insert(0, str(self.loop_mode))
+
+        self.can_img.grid(column=1, row=0, padx=5, pady=5, columnspan=2)
+        self.but_add.grid(column=1, row=1, padx=5, pady=10, columnspan=2)
+        self.lab_dur.grid(column=1, row=2, pady=5, sticky="E")
+        self.ent_dur.grid(column=2, row=2, padx=10, pady=5, sticky="W")
+        self.lab_loop.grid(column=1, row=3, pady=5, sticky="E")
+        self.ent_loop.grid(column=2, row=3, padx=10, pady=5, sticky="W")
+        self.but_save.grid(column=1, row=5, padx=5, pady=5, sticky="N")
+        self.but_cancel.grid(column=2, row=5, padx=5, pady=5, sticky="N")
+
+        self.can_but.grid(column=3, row=1, sticky="NS", padx=5, pady=5)
+
+
+        self.yscroll = ttk.Scrollbar(self.top, orient=VERTICAL)
+        self.yscroll.grid(column=2, row=1, sticky="NS")
+
+        self.res_box = Listbox(self.top, width=50, height=23,
+                                yscrollcommand=self.yscroll.set)
+        self.res_box.grid(column=1, row=1, padx=10, pady=10)
+
+        self.yscroll.config(command=self.res_box.yview)
+
+if __name__ == '__main__':
+
+    root = Tk()
+    d = MyDialogGif(root)
+    root.wait_window(d.top)
